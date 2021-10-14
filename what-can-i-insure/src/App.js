87251e643e03ad823bc1f9619d6b2b650c7acb67
jonasskabgl <91899657+jonasskabgl@users.noreply.github.com>
2021-10-14 14:33:33 +0100

    added ctm logo

--- what-can-i-insure/src/App.js
+++ what-can-i-insure/src/App.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -44,9 +42,7 @@ export default function SignIn() {
                         alignItems: 'center',
                     }}
                 >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
+                    <img src={"/CTM Logo.png"} />
                     <Typography component="h1" variant="h5">
                         What cars can you insure?
                     </Typography>
