7fc3340b105f595c4765eee32bf3de6b138f396f
Jose Neto <zecarrera@gmail.com>
2021-10-14 13:28:00 +0100

    QT-hackathon Filters by age and postcode

--- what-can-i-insure/server/database.js
+++ what-can-i-insure/server/database.js
@@ -8,7 +8,7 @@ const lookupData = [{
     "DateOfBirth": "1960-10-20",
     "Make": "HONDA",
     "Model": "VFR 800 V4 VTECH",
-    "postcode": "PE4 6BS",
+    "postcode": "PE4 7BS",
     "annualPremium": 120.9
 },{
     "DateOfBirth": "2004-02-20",
@@ -22,6 +22,40 @@ const lookupData = [{
     "Model": "CORSA",
     "postcode": "LE2 7BG",
     "annualPremium": 600.9
-}]
+},
+{
+    "DateOfBirth": "2003-7-20",
+    "Make": "Ford",
+    "Model": "Focus",
+    "postcode": "PE4 8BS",
+    "annualPremium": 752
+},{
+    "DateOfBirth": "2003-7-12",
+    "Make": "Ford",
+    "Model": "Focus",
+    "postcode": "PE4 6BS",
+    "annualPremium": 820
+},{
+    "DateOfBirth": "2003-1-03",
+    "Make": "Ford",
+    "Model": "Focus",
+    "postcode": "PE4 6BS",
+    "annualPremium": 764
+},
+{
+    "DateOfBirth": "2003-1-03",
+    "Make": "Ford",
+    "Model": "Focus",
+    "postcode": "PE13 6BS",
+    "annualPremium": 745
+},
+{
+    "DateOfBirth": "2003-1-03",
+    "Make": "Ford",
+    "Model": "Focus",
+    "postcode": "PE11 6BS",
+    "annualPremium": 710
+}
+]
 
 module.exports = {lookupData};
--- what-can-i-insure/server/routes/insurance.js
+++ what-can-i-insure/server/routes/insurance.js
@@ -4,13 +4,38 @@ const {lookupData} = require("../database")
 
 /* GET root. */
 router.get('/insurance/:postcode/:age', function(req, res) {
-
     const postcode = req.params?.postcode;
     const age = req.params?.age;
-
+    
     res.contentType("application/json");
-    res.status(200).json({postcode,age,lookupData});
+    dateOfBirthToAge(lookupData[0].DateOfBirth);
+    const matchingDataByPostcode = matchByPostcode(postcode);
+    if (matchingDataByPostcode.length > 0){
+        let matchingData = matchByAge(age, matchingDataByPostcode)
+        if (matchingData.length > 0) {
+            res.status(200).json({postcode,age,matchingData});
+        }
+    }
+    res.sendStatus(204);
 });
+
+const matchByPostcode = postcode => {
+    const outerPostcode = postcode.split(' ')[0];
+    const matchesByPostcode = lookupData.filter(data => data.postcode.split(' ')[0] === outerPostcode)
+
+    return matchesByPostcode;
+}
+
+const matchByAge = (queryAge, matchesByPostcode, tolerance) => {
+    return matchesByPostcode.filter(data => dateOfBirthToAge(data.DateOfBirth) === Number(queryAge));
+}
+
+const dateOfBirthToAge = dateOfBirth => {
+    const today = new Date();
+    const birthDate = new Date(dateOfBirth);
+    const age = today.getFullYear() - birthDate.getFullYear();
+    return age;
+}
 
 
 module.exports = router;
