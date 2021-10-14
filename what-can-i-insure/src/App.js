e88b9e8ee03b054c5d8af162a85a07c6a2ac6f43
jonasskabgl <91899657+jonasskabgl@users.noreply.github.com>
2021-10-14 14:23:40 +0100

    added some front end capabilities

--- what-can-i-insure/src/App.js
+++ what-can-i-insure/src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Axios from "axios";
+import {Table, TableRow, TableCell, TableHead} from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            Disclaimer: this is estimated data based on your area, and not an offer of insurance.
+        </Typography>
+    );
 }
 
-export default App;
+const theme = createTheme();
+
+export default function SignIn() {
+
+    const [postcode,setPostcode] = React.useState("");
+    const [age, setAge] = React.useState("");
+    const [results, setResults] = React.useState([]);
+
+    const getQuotes = async () => {
+        const response = await Axios.get(`http://localhost:3001/insurance/${postcode}/${age}`)
+        setResults(response.data);
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        What cars can you insure?
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="postcode"
+                            label="Postcode"
+                            name="postcode"
+                            autoComplete="postcode"
+                            autoFocus
+                            onChange={(event) => setPostcode(event.target.value)}
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="age"
+                            label="Age"
+                            id="age"
+                            onChange={(event) => setAge(event.target.value)}
+                        />
+                        <Button
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            onClick={getQuotes}
+                        >
+                            Find Cars
+                        </Button>
+
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+                {results.length>0 ? <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                Make
+                            </TableCell>
+                            <TableCell>
+                                Model
+                            </TableCell>
+                            <TableCell>
+                                Average premium
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    {results.map((car, index) => {
+                        <TableRow key={index}>
+                            <TableCell>
+                                {car.Make}
+                            </TableCell>
+                            <TableCell>
+                                {car.Model}
+                            </TableCell>
+                            <TableCell>
+                                {car.annualPremium}
+                            </TableCell>
+                        </TableRow>
+                    })}
+                </Table>:null}
+
+            </Container>
+        </ThemeProvider>
+    );
+}
